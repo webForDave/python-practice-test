4633608ce2568cdbf8a4a68a1b70e82da9c27dbc
David <dakinola54@gmail.com>
2024-01-07 05:18:40 -0800

    "modified game"

---
+++ test.py
@@ -0,0 +1,15 @@
+number = 50
+guess = int(input('Enter a number : '))
+
+while guess :
+    if guess < number :
+        print('Too low')
+
+    elif guess > number : 
+        print('Too high')
+
+    elif guess == number :
+        print('correct')
+        break
+
+    guess = int(input('Enter a number : '))
